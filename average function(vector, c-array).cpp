48a421be81dc9e9e1c5b733a5e4bba37dad1b69f
DogFooter <tmflffj4663@gmail.com>
2023-09-09 12:54:36 +0900

    template average 함수 수정

--- average function(vector, c-array).cpp
+++ average function(vector, c-array).cpp
@@ -8,18 +8,23 @@ average function(vector, c-array)
 */
 template<typename T>
 typename T::value_type average(T& stlContainer) {
+    if (stlContainer.empty()) return 0; // prevent division by zero
 
-    return accumulate(stlContainer.begin(), stlContainer.end(), 0);
+    typename T::value_type sum = std::accumulate(stlContainer.begin(), stlContainer.end(), 0);
+    return sum / stlContainer.size(); // calculate average by dividing the sum by the number of elements
+
 }
 
 template<typename T, int N>
 T average(T(&ptr)[N]) {
+    if (N == 0 ) return 0; // prevent division by zero
+
     T sum = 0;
     //cout << __FUNCSIG__ << endl;
     for (T a : ptr)
         sum += a;
 
-    return sum;
+    return sum / N ;
 }
 /* test code */
 int main() {
