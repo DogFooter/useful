59289ae64d24ac60bdee9164305b86e286a8a640
DogFooter <tmflffj4663@gmail.com>
2023-09-11 23:09:21 +0900

    arraysClear 컨테이너 초기화 추가

--- ContainerClear.h
+++ ContainerClear.h
@@ -6,3 +6,17 @@ void vectorsClear(Ts& ... args) {
 	(args.clear(), ...);
 
 };
+
+
+// Clear arrays
+template<typename T, size_t N>
+void arrayClear(T(&arr)[N]) {
+	for (size_t i = 0; i < N; ++i) {
+		arr[i] = T{};
+	}
+}
+
+template<typename... Ts, size_t... Ns>
+void arraysClear(Ts(&... args)[Ns]) {
+	((arrayClear(args)), ...);
+}
