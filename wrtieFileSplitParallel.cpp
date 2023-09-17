0b7da3089a1b57d195516461bdefa59536ca63ff
DogFooter <tmflffj4663@gmail.com>
2023-09-17 23:14:30 +0900

    fileParallel class

--- wrtieFileSplitParallel.cpp
+++ wrtieFileSplitParallel.cpp
@@ -3,37 +3,38 @@
 #include <thread>
 #include <fstream>
 #include <string>
+class fileParallel {
+    void writeToFile(const std::vector<short>& data, size_t start, size_t end, int fileIndex) {
+        std::string filename = "file_" + std::to_string(fileIndex + 1) + ".dat";
+        FILE* file = fopen(filename.c_str(), "wb");
 
-void writeToFile(const std::vector<short>& data, size_t start, size_t end, int fileIndex) {
-    std::string filename = "file_" + std::to_string(fileIndex + 1) + ".dat";
-    FILE* file = fopen(filename.c_str(), "wb");
-
-    if (file != nullptr) {
-        fwrite(&data[start], sizeof(short), end - start, file);
-        fclose(file);
-    }
-}
-
-void writeDataParallel(std::vector<short>& data, size_t bytesPerFile) {
-    const size_t shortsPerFile = bytesPerFile / sizeof(short);
-
-    // Calculate number of files
-    int numFiles = (data.size() + shortsPerFile - 1) / shortsPerFile;
-
-    // Create and manage threads
-    std::vector<std::thread> threads;
-
-    for (int i = 0; i < numFiles; ++i) {
-        size_t start = i * shortsPerFile;
-        size_t end = std::min(start + shortsPerFile, data.size());
-
-        threads.emplace_back(writeToFile, std::ref(data), start, end, i);
-
+        if (file != nullptr) {
+            fwrite(&data[start], sizeof(short), end - start, file);
+            fclose(file);
+        }
     }
 
-    // Wait for all threads to finish
-    for (auto& t : threads) {
-        t.join();
+    void writeDataParallel(std::vector<short>& data, size_t bytesPerFile) {
+        const size_t shortsPerFile = bytesPerFile / sizeof(short);
+
+        // Calculate number of files
+        int numFiles = (data.size() + shortsPerFile - 1) / shortsPerFile;
+
+        // Create and manage threads
+        std::vector<std::thread> threads;
+
+        for (int i = 0; i < numFiles; ++i) {
+            size_t start = i * shortsPerFile;
+            size_t end = std::min(start + shortsPerFile, data.size());
+
+            threads.emplace_back(writeToFile, std::ref(data), start, end, i);
+
+        }
+
+        // Wait for all threads to finish
+        for (auto& t : threads) {
+            t.join();
+        }
     }
 }
 
